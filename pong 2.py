77c5300df8028f21689b0511ca25de938b84a818
marcos-alm <contato.rodrigz.alm@@gmail.com>
2024-06-25 09:05:55 -0300

    comentários adicionais no inicio do programa

--- pong 2.py
+++ pong 2.py
@@ -1,12 +1,20 @@
+# mar.almeida@discente.ufma.br
+# Marcos André Rodrigues Almeida
+# 25.06.24 (9:00 am)
+# 2024.1
+
+# importando as bibliotecas
 import pygame
 from pygame.locals import *
 from sys import exit
 
+#iniciando o pygame
 pygame.init()
  
  #tela (janela)
 largura = 640
 altura = 480
-
 janela = pygame.display.set_mode((largura, altura))
+
+#nome do jogo no canto superior direito da tela
 pygame.display.set_caption('Pong 2: O Atirador')
